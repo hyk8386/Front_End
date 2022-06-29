d2efadcde88e3e9eeab5c57f1add5be17b5de921
DESKTOP-GOC4BUG\505 <hyk8386@naver.com>
2022-06-29 17:56:28 +0900

    220629_JavaScript

---
+++ 0629/test01_1.js
@@ -0,0 +1,11 @@
+// 문자열 자르기_2 - Substr
+
+const sentence = 'Wakanda Forever!!!';
+
+console.log(sentence.substr(3));
+console.log(sentence.substr(3,7));
+console.log(sentence.substr(0));
+console.log(sentence.substr(0,-23));
+console.log(sentence.substr(50));
+console.log(sentence.substr(7,2));
+
